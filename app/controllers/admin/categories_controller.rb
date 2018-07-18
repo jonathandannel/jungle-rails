45c4c3622642b692edac4ad91d7849f92698aa75
jonathandannel <usixjad@gmail.com>
2018-07-18 16:51:59 +0000

    fully implemented admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,12 +4,25 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def category_params
     params.require(:category).permit(
-      :name,
-      :description
+      :name
     )
   end
 
