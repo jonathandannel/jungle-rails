54bd59a5add12d41326504b35af62ad8da260401
jonathandannel <usixjad@gmail.com>
2018-07-19 22:09:46 +0000

    added email functionality for order confirmation

---
+++ app/mailers/orders_mailer.rb
@@ -0,0 +1,8 @@
+class OrdersMailer < ApplicationMailer
+
+  def receipt_email(order_id)
+    @order = Order.find_order_with_items(order_id)
+    mail(to: @order.email, subject: "Jungle Order Confirmation ##{order.id}")
+  end
+
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,13 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :send_receipt
+
+  def self.find_order_with_items(id)
+    self.includes(line_items: [:product]).find(id)
+  end
+
+  def send_receipt
+    OrdersMailer.receipt_email(self.id).deliver_later
+  end
 end
