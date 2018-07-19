a59cbfac41357282c90eeb1116fe205b7a54bfb0
jonathandannel <usixjad@gmail.com>
2018-07-19 22:18:47 +0000

    fixed typo with actionmailer

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -2,7 +2,7 @@ class OrdersMailer < ApplicationMailer
 
   def receipt_email(order_id)
     @order = Order.find_order_with_items(order_id)
-    mail(to: @order.email, subject: "Jungle Order Confirmation ##{order.id}")
+    mail(to: @order.email, subject: "Jungle Order Confirmation ##{@order.id}")
   end
 
 end
